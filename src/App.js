2f86dca4f5a270e6f480bb95df6b14cce0a49f0d
unknown <saketkr2k11@gmail.com>
2020-09-11 13:44:50 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
+import './App.css';
+
+function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios.get('https://5peim4ixwj.execute-api.us-east-2.amazonaws.com/development/');
+      console.log(res.data);
+      setData(res.data);
+    }
+    fetchData();
+  }, []);
+  const apiData = data.map((x) =>
+    <div style={{ margin: '20px' }}>
+      <div key={x.id}>{'Url: '}{x.url}</div>
+      <div>{'name:'}{x.name}</div>
+    </div>)
+  return (
+    <div className="App">
+      {apiData}
+    </div>
+  );
+}
+
+export default App;
